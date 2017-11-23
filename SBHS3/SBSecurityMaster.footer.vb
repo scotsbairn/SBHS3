a4583e74c5affc62032b90ac8436f02f77b708a0
Eric Chesters <eric@scotsbairn.com>
2017-11-22 17:42:00 -0800

    updates

--- SBHS3/SBSecurityMaster.footer.vb
+++ SBHS3/SBSecurityMaster.footer.vb
@@ -18,6 +18,13 @@ End Sub
 
 
 Sub StatusChangeCB(ByVal Parm As Object())
+   If Parm Is Nothing Then Exit Sub
+   If Parm.Length < 5 Then Exit Sub
+
+   Dim Ref As Integer = Parm(4)
+
+   Dim sv As New SkyView(hs)
+   
    hs.WriteLog("SBSecurityMaster", "hello")
 
    Dim sv As New SkyView(hs)
